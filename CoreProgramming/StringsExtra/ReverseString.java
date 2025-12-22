849cfdcef6a6e0dabb3418663983ab314ebbeceb
preethi-r04 <preethiliki2004@gmail.com>
2025-12-22 17:26:48 +0530

    [Preethi]: Added StringsExtra,BuitinExtras

---
+++ CoreProgramming/StringsExtra/ReverseString.java
@@ -0,0 +1,13 @@
+package CoreProgramming.StringsExtra;
+import java.util.Scanner;
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = input.nextLine();
+        for ( int i = text.length() - 1; i >= 0; i-- ) {
+            System.out.print(text.charAt(i));
+        }
+        input.close();
+    }
+}
---
+++ CoreProgramming/StringsExtra/Substring.java
@@ -0,0 +1,19 @@
+package CoreProgramming.StringsExtra;
+import java.util.Scanner;
+public class Substring {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = input.nextLine();
+        System.out.print("Enter the substring to search: ");
+        String substring = input.nextLine();
+        int count = 0;
+        int index = 0;
+        while ((index = text.indexOf(substring, index)) != -1) {
+            count++;
+            index += substring.length();
+        }
+        System.out.println("Occurrence of substring: " + count);
+    input.close();
+    }
+}
